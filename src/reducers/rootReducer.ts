f19fcc628754c7a30191498071754c63079fb765
Rani <ranisans@yahoo.com>
2020-03-15 14:11:56 +0300

    refact: remove size's action and reducer

--- src/reducers/rootReducer.ts
+++ src/reducers/rootReducer.ts
@@ -1,14 +1,12 @@
 import { combineReducers } from 'redux';
 
 import { sortingReducer } from './sortingReducer';
-import { sizeReducer } from './sizeReducer';
 import { changeTableType } from './tableTypeReducer';
 import { filterReducer } from './filterReducer';
 
 const rootReducer = combineReducers({
   sorting: sortingReducer,
   filter: filterReducer,
-  size: sizeReducer,
   tableType: changeTableType,
 });
 
