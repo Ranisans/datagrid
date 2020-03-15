3f7d1edb13355e2174edfb72f863e754f746db5e
Rani <ranisans@yahoo.com>
2020-03-15 14:08:57 +0300

    feat: add Redux DevTools Extension config don't show change state in the browser - bug?

--- src/store.ts
+++ src/store.ts
@@ -1,9 +1,13 @@
-import { createStore } from 'redux';
+import { createStore, applyMiddleware } from 'redux';
+import { composeWithDevTools } from 'redux-devtools-extension';
 
 import rootReducer from './reducers/rootReducer';
-
+// FIX redux-devtools not working - don't show change state in the browser
 const store = createStore(
   rootReducer,
+  composeWithDevTools(
+    applyMiddleware(),
+  ),
 );
 
 export default store;
