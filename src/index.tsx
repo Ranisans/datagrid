0ba9c92b804d344bae46380a49e3640ba7557e98
Ranisans <ranisans@yahoo.com>
2020-03-17 14:11:45 +0300

    fix: add polyfill for  Map and Set fo redux-devtools

--- src/index.tsx
+++ src/index.tsx
@@ -3,4 +3,12 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 
+// redux-devtools not work with Set and Map
+(Set.prototype as any).toJSON = function setProt() {
+  return Array.from(this);
+};
+(Map.prototype as any).toJSON = function mapProt() {
+  return JSON.parse(JSON.stringify([...this]));
+};
+
 ReactDOM.render(<App />, document.getElementById('root'));
--- src/store.ts
+++ src/store.ts
@@ -2,7 +2,7 @@ import { createStore, applyMiddleware } from 'redux';
 import { composeWithDevTools } from 'redux-devtools-extension';
 
 import rootReducer from './reducers/rootReducer';
-// FIX redux-devtools not working - don't show change state in the browser
+
 const store = createStore(
   rootReducer,
   composeWithDevTools(
