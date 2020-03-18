d97df17eb0669eb9fba83ca2ade8612bc7402765
Ranisans <ranisans@yahoo.com>
2020-03-18 08:19:48 +0300

    Updates

---
+++ precache-manifest.0be8b0e511beb17e74d348db4d29b18c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "276e178dde642a7797428f7fe58b3723",
+    "url": "/datagrid/index.html"
+  },
+  {
+    "revision": "fbecdd6db58f0d129f2f",
+    "url": "/datagrid/static/css/main.bfcd9915.chunk.css"
+  },
+  {
+    "revision": "e96e70f2baa7657adcb4",
+    "url": "/datagrid/static/js/2.52a92e33.chunk.js"
+  },
+  {
+    "revision": "ac9fac6c8e755a8efecd01981adb88e6",
+    "url": "/datagrid/static/js/2.52a92e33.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fbecdd6db58f0d129f2f",
+    "url": "/datagrid/static/js/main.7bb25d79.chunk.js"
+  },
+  {
+    "revision": "8d91fbbb6eacedc04f54",
+    "url": "/datagrid/static/js/runtime-main.0cc68fbb.js"
+  }
+]);
